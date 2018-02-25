d73204c14f5de7627e259bc3591bc79fbb1528e8
Craig <coder2169@gmail.com>
2018-02-25 12:18:54 -0700

    gulp-setup

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require("gulp");
+var watch = require("gulp-watch");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var simplevars = require("postcss-simple-vars");
+
+
+gulp.task("default",function(){
+  console.log("gulp running");
+});
+gulp.task("styles",function(){
+  return gulp.src("./app/assets/styles/styles.css")
+  .pipe(postcss([simplevars,autoprefixer]))
+  .pipe(gulp.dest("./app/temp/styles"));
+});
+gulp.task("html",function(){
+  console.log("index.html modification!!");
+});
+gulp.task("watch",function(){
+  watch("./app/index.html",function(){
+    gulp.start("html");
+  });
+  watch("./app/assets/styles/**/*.css",function(){
+    gulp.start("styles")
+  });
+});
