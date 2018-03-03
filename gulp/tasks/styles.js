482cadf1658bb270287763ee6e9c9ef853fd1a80
Craig <coder2169@gmail.com>
2018-03-02 22:54:50 -0700

    mobile-edits

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,9 +4,10 @@ var autoprefixer = require("autoprefixer");
 var simplevars = require("postcss-simple-vars");
 var nested = require("postcss-nested");
 var cssimport = require("postcss-import");
+var cssmixins = require("postcss-mixins");
 
 gulp.task("styles",function(){
   return gulp.src("./app/assets/styles/styles.css")
-  .pipe(postcss([cssimport,simplevars,nested,autoprefixer]))
+  .pipe(postcss([cssimport,cssmixins,simplevars,nested,autoprefixer]))
   .pipe(gulp.dest("./app/temp/styles"));
 });
